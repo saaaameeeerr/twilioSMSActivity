bf3fc81bdef06db47efa712f30f046ff6081f8ea
Aviralkakkar <49982744+Aviralkakkar@users.noreply.github.com>
2020-11-25 16:57:54 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -5,7 +5,7 @@ var util = require('util');
 const Path = require('path');
 const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
 var http = require('https');
-const http = require("http");
+//const http = require("http");
 
 exports.logExecuteData = [];
 
