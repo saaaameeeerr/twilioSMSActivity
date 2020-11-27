713532df73e12e309983daf1d86671bc3587e204
Aviralkakkar <49982744+Aviralkakkar@users.noreply.github.com>
2020-11-27 15:33:41 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -144,8 +144,8 @@ exports.execute = function(req, res) {
             "account_id": "514003869"
             }
            },function(error, response, body)  {
-            
-                console.log(body.access_token)
+                console.log(body);
+                console.log(body.access_token);
             
           } )
           
